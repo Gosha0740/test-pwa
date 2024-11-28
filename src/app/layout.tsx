440894727055a7a87fe6c188813f1ff88d73d41f
Gosha <gosha0740@gmail.com>
2024-11-28 06:58:37 +0300

    123

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,11 +32,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <link rel="manifest" href="/manifest.json" />
-        <link rel="icon" href="/icons/icon512_rounded.png" />
-        <meta name="theme-color" content="#000000" />
-      </head>
       <body
         className={`antialiased`}
       >
