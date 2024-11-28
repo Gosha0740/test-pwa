f638fa4f31580f5210dddf47e64e7861d337fd09
Gosha <gosha0740@gamail.com>
2024-11-28 06:42:37 +0300

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,8 @@ import { Button } from '@/components/ui/button'
 export default function Home() {
   return (
     <div>
-      <Button onClick={() => MSG()}>Button - 2</Button>
+      <Button onClick={() => alert('Hello')}>Button - 2</Button>
     </div>
   )
 }
 
-function MSG() {
-  alert('Hello')
-}
